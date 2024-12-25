444753a6c6eef189f15556e650ce96978634291b
atharva <atharva.chirde@gmail.com>
2024-12-25 18:10:10 +0530

    tailwindCSS

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  c;
+
+  return (
+    <>
+      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center gap-x-4">
+        <div class="shrink-0">
+          <img class="size-12" src="/img/logo.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div class="text-xl font-medium text-black">ChitChat</div>
+          <p class="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
+      <button class="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
+    </>
+  );
+}
+
+export default App;
